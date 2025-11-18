574a64147c6b8defd25b9dda2f261cd8ffe5f7c9
DannaAlbertos <danna.albertos@tecdesoftware.edu.mx>
2025-11-18 17:26:58 -0600

    ejercicio 2.3 resuelto

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -314,6 +314,20 @@ function invertirColores(matriz) {
  */
 function convertirEscalaGrises(matriz) {
   // TODO: Implementar conversión a escala de grises
+  const resultado = copiarMatriz(matriz);
+
+  for (let i = 0; i< resultado.length; i++){
+    for ( let j = 0; j< resultado[i].length; j++){
+      const px = matriz [i][j]; 
+      const gris = Math.round(0.299 * px.r + 0.587 * px.g + 0.114 * px.b);
+      resultado[i][j]= {
+        r: gris,
+        g: gris,
+        b: gris,
+        a: px.a
+      };
+    } 
+  }
   
   // Para cada pixel:
   // 1. Calcular el valor de gris
@@ -322,7 +336,7 @@ function convertirEscalaGrises(matriz) {
   // 2. Asignar ese valor a los tres canales
   // pixelNuevo = {r: gris, g: gris, b: gris, a: pixel.a}
   
-  return []; // REEMPLAZAR
+  return resultado; // REEMPLAZAR
 }
 
 // ============================================
