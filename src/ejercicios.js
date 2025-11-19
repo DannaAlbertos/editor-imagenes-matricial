2b9a1ac9e3fd592283d0c745b776d3f48962837d
DannaAlbertos <danna.albertos@tecdesoftware.edu.mx>
2025-11-19 11:21:31 -0600

    test

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,9 +1,9 @@
 // ============================================
 // EDITOR DE IMÁGENES CON ÁLGEBRA MATRICIAL
 // ============================================
-// Nombre del estudiante: _________________
-// Fecha: _________________
-// Grupo: _________________
+// Nombre del estudiante: Danna Pamela ALbertos Sosa
+// Fecha:  18/11/2025
+// Grupo: "A"
 
 const { PNG } = require('pngjs');
 const fs = require('fs');
